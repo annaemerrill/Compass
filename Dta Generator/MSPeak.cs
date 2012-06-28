e744d0b1522105da9fd4450934b3f4b3ed3c6b81
Derek Bailey <dbaileychess@gmail.com>
2012-06-28 12:05:26 -0500

    initial project version

---
+++ Dta Generator/MSPeak.cs
@@ -0,0 +1,29 @@
+﻿namespace DtaGenerator
+{
+    public class MSPeak
+    {
+        protected double mz;
+
+        public double MZ
+        {
+            get { return mz; }
+            set { mz = value; }
+        }
+
+        protected double intensity;
+
+        public double Intensity
+        {
+            get { return intensity; }
+            set { intensity = value; }
+        }
+
+        public MSPeak() { }
+
+        public MSPeak(double mz, double intensity)
+        {
+            this.mz = mz;
+            this.intensity = intensity;
+        }
+    }
+}
---
+++ Dta Generator/ProgressEventArgs.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace DtaGenerator
+{
+    public class ProgressEventArgs : EventArgs
+    {
+        private int _progress;
+
+        public int Progress
+        {
+            get { return _progress; }
+            set { _progress = value; }
+        }
+
+        public ProgressEventArgs(int progress)
+        {
+            _progress = progress;
+        }
+    }
+}
